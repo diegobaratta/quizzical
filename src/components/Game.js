f022e1740f899c966c945e2a50eb981f29855f94
Diego Baratta <diego.f.baratta@gmail.com>
2022-07-28 19:50:22 -0300

    Game working

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,42 +7,118 @@ function Game() {
 
     async function getQuestions() {
 
-        const resp = await fetch("https://opentdb.com/api.php?amount=10")
+        const resp = await fetch("https://opentdb.com/api.php?amount=1")
         const data = await resp.json()
 
-        setQuestions(data.results)
+        const queArr = await data.results.map(({question, correct_answer, incorrect_answers}) => {
+                    
+            //Craft the array with all the answers
+            const answers = incorrect_answers.map(answer => {
+                                return ({
+                                    id: nanoid(),
+                                    answer: answer,
+                                    val: false,
+                                    userInput: false,
+                                })
+                            })
+    
+            answers.push({
+                id: nanoid(),
+                answer: correct_answer,
+                val: true,
+                userInput: false,
+            })                
+            
+            //Suffle the array for avoid that the last one always be the correct one 
+            answers.sort((a, b) => 0.5 - Math.random())
+
+            return ({
+                question: question, 
+                answers: answers,                
+            })
+        })
+
+        setQuestions(queArr)
     }
 
     useEffect(() => {
         getQuestions()
     }, [])
 
-    return (
-        <ol>
-            {questions.map(({question, correct_answer, incorrect_answers}) => {
-                incorrect_answers.push(correct_answer)
+    function decodeHTMLCode(str){
+        return str.replace(/&amp;/g, "&")
+                    .replace(/&quot;/g, "'")
+                    .replace(/&deg;/g, "°")
+                    .replace(/(&#(\d+);)/g, (match, capture, charCode) => 
+                        String.fromCharCode(charCode));
+    }
 
-                const answers = {
-                    
-                }
+    function handleClick(src) {
+
+        switch (src) {
+            case 'shuffle':
+                getQuestions()
+                break;
+            case 'check':
+                break;
+            default:
+                break;    
+        }
+    }
+
+    function selectAnswer(id) {
+        
+        setQuestions(prevQue => prevQue.map( ({question, answers}) => {
+            console.log(question)
+            const modAns = answers.map ( oldAnswer => {
                 
-                console.log(incorrect_answers.sort((a, b) => 0.5 - Math.random()));
+                return id === oldAnswer.id ? 
+                        {
+                            ...oldAnswer,
+                            userInput: true
+                        } :
+                        {
+                            ...oldAnswer,
+                            userInput: false
+                        }
+            })
 
-                //Correct answer added to the incorrects one and then I shuffle the array.
-                //const answers = incorrect_answers.push(correct_answer).sort((a, b) => 0.5 - Math.random());
+            return {
+                ...question, 
+                answer: modAns,
+            }
+        }))
 
-                /*return (
-                    <li key={nanoid()}>
-                        <h3>{question}</h3>
-                        <ul>
-                            {answers.map(answer => {
-                                return (<li key={nanoid()}>{answer}</li>)
-                            })}
-                        </ul>
-                    </li>
-                )*/
-            })}
-        </ol>
+    }
+
+    const questionlist = questions.map(({answers, question}) => {
+
+        return (
+            <li className='question-group' key={nanoid()}>
+                <h3 className='question'>{decodeHTMLCode(question)}</h3>
+                <ul className='answer-group'>
+                    {answers.map(answer => {
+                        return (
+                            <li className='answer' key={nanoid()} onClick={() => selectAnswer(answer.id)}>
+                                {decodeHTMLCode(answer.answer)}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </li>
+        )
+    })
+
+    return (
+        <section className='game'>
+            <ol>
+                {questionlist}
+            </ol>
+            <div className='game-buttons'>
+                <button className='btn' onClick={() => handleClick('shuffle')}>Shuffle Questions</button>
+                <button className='btn' onClick={() => handleClick('check')}>Check answers</button>
+            </div>
+        </section>
     )
 }
 
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -10,7 +10,7 @@ export default function MainMenu(props) {
         <>
             <h1 className='main--title'>Quizzical</h1>
             <h3 className='main--subtext'>Your friendly trivia game</h3>
-            <button className='main--btn' onClick={handleStart}>Start quiz</button>
+            <button className='btn' onClick={handleStart}>Start quiz</button>
         </>
     )
 }
